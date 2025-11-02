532cea32c4ea56ba72bc800163b6e7dd88b3c9fa
Adixtin <adam@at3.pl>
2025-11-02 16:05:50 +0000

    added a component for login plus setting up the cookie

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+
+import Login from "$lib/assets/components/Login.svelte";
+</script>
+
+<main>
+    <Login></Login>
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        width: 100%;
+    }
+</style>
