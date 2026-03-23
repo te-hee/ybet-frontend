b80611341ec5a423a08e804d516411a4b7f4d42a
Bartek <u117_barcud_waw@technischools.com>
2026-03-23 11:12:58 +0100

    reorganize component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,38 @@
-import React from 'react';
-import { AuthProvider, useAuth } from './authContext.tsx';
-import Login from './assets/components/Login.tsx';
-import Chat from './assets/components/Chat.tsx';
-import Input from './assets/components/Input.tsx';
+import React from "react";
+import { AuthProvider, useAuth } from "./authContext.tsx";
+import Login from "./assets/components/Login/index.tsx";
+import Chat from "./assets/components/Chat/index.tsx";
+import Input from "./assets/components/Input/index.tsx";
 
-import './App.css'
-import ActiveUsers from "./assets/components/ActiveUsers.tsx";
-
+import "./App.css";
+import ActiveUsers from "./assets/components/ActiveUsers/index.tsx";
 
 const AppContent: React.FC = () => {
-    const { token } = useAuth();
+  const { token } = useAuth();
 
-    return (
-        <main>
-            {!token ? (
-                <Login />
-            ) : (
-                <>
-                    <div className='left'>
-                        <Chat limit={20}/>
-                        <Input/>
-                    </div>
-                    <div className='right'>
-                        <ActiveUsers users={['ja', 'on']}></ActiveUsers>
-                    </div>
-                </>
-            )}
-        </main>
-    );
+  return (
+    <main>
+      {!token ? (
+        <Login />
+      ) : (
+        <>
+          <div className="left">
+            <Chat limit={20} />
+            <Input />
+          </div>
+          <div className="right">
+            <ActiveUsers users={["ja", "on"]}></ActiveUsers>
+          </div>
+        </>
+      )}
+    </main>
+  );
 };
 
 export default function App() {
-    return (
-        <AuthProvider>
-            <AppContent />
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <AppContent />
+    </AuthProvider>
+  );
 }
